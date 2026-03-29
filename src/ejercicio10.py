54c1f25451a983f804282dc7a39e632cad31fb19
valen19g <valen2002gallimalaret@gmail.com>
2026-03-29 02:06:53 -0300

    actividad 2 correcion

--- src/ejercicio10.py
+++ src/ejercicio10.py
@@ -79,9 +79,34 @@ rounds = [
 
 puntajemaximo=-1
 ganador=""
-for participante in rounds[0]["scores"]:
-    datos=rounds[0]["scores"][participante]
-    puntuacion_total=datos["judge_1"]+datos["judge_2"]+datos["judge_3"]
-    if puntuacion_total>puntajemaximo:
-        puntajemaximo=puntuacion_total
-        ganador=datos=rounds[0]["scores"][0]
+numero_ronda=1
+estadisticas={}
+
+for ronda in rounds:
+    print(f"Ronda {numero_ronda} - {ronda['theme']}:")
+    for participante in ronda["scores"]: 
+        datos=ronda["scores"][participante] 
+        if participante not in estadisticas:  
+           estadisticas[participante]={
+           "total": 0,
+           "ganadas": 0,
+           "mejor": 0
+           }
+        puntuacion_total=datos["judge_1"]+datos["judge_2"]+datos["judge_3"] 
+        if puntuacion_total>puntajemaximo: 
+            puntajemaximo=puntuacion_total 
+            ganador=participante 
+        estadisticas[participante]["total"]+=puntuacion_total
+        if puntuacion_total > estadisticas[participante]["mejor"]:
+            estadisticas[participante]["mejor"]=puntuacion_total
+    estadisticas[ganador]["ganadas"]+=1
+    print(f'ganador: {ganador} ({puntajemaximo} pts) ')    
+    numero_ronda+=1
+    puntajemaximo=-1
+    ganador=""
+lista = list(estadisticas.items())
+lista.sort(key=lambda x: x[1]["total"], reverse=True)
+for nombre,datos in lista:
+    promedio=datos['total']/len(rounds)
+    print(f'{nombre} - {datos} promedio {promedio}')
+    
